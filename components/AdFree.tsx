e4067a3c0a7e6f353b5e93fcd03c24d12fa0b0fb
oavaDev <omarvega598@gmail.com>
2023-04-05 18:28:58 -0500

    feat(offline): add offline section

--- components/AdFree.tsx
+++ components/AdFree.tsx
@@ -1,4 +1,3 @@
-import PageWrapper from '@/app/page-wrapper';
 import Image from 'next/image';
 import React from 'react';
 
@@ -6,34 +5,32 @@ const AdFree = () => {
   const IMAGE_URL =
     'https://res.cloudinary.com/dj80e8qqp/image/upload/v1680730214/billie_onzu7n.jpg';
   return (
-    <PageWrapper>
-      <article className='w-screen bg-white h-screen  '>
-        <div className=' w-screen h-screen flex flex-col md:flex-row items-center justify-around my-auto'>
-          <aside className=''>
-            <figure className='absolute w-[250px] h-[290px] z-5 -my-6 -mx-14 border-2 border-black'></figure>
-            <Image
-              className='relative z-5'
-              src={IMAGE_URL}
-              width={250}
-              loading='lazy'
-              height={250}
-              quality={100}
-              alt='billie'
-            />
-          </aside>
-          <aside className='flex flex-col gap-5'>
-            <h2 className='text-black text-5xl font-bold w-80'>
-              Unlimited, ad-free music
-            </h2>
-            <p>
-              No ads, No interrumptions.
-              <br />
-              Just music.
-            </p>
-          </aside>
-        </div>
-      </article>
-    </PageWrapper>
+    <article className='w-screen bg-white h-screen  '>
+      <div className=' w-screen h-screen flex flex-col md:flex-row text-center items-center justify-around my-auto'>
+        <aside>
+          <figure className='absolute w-[250px] h-[290px] z-5 -my-6 -mx-14 border-2 border-black'></figure>
+          <Image
+            className='relative z-5'
+            src={IMAGE_URL}
+            width={250}
+            loading='lazy'
+            height={250}
+            quality={100}
+            alt='billie'
+          />
+        </aside>
+        <aside className='flex flex-col gap-5'>
+          <h2 className='text-black text-5xl font-bold w-80'>
+            Unlimited, ad-free music
+          </h2>
+          <p>
+            No ads, No interrumptions.
+            <br />
+            Just music.
+          </p>
+        </aside>
+      </div>
+    </article>
   );
 };
 
--- components/Offline.tsx
+++ components/Offline.tsx
@@ -1,7 +1,75 @@
+import Image from 'next/image';
 import React from 'react';
-
+import { AiFillPlayCircle } from 'react-icons/ai';
 const Offline = () => {
-  return <div className='w-screen bg-[#93438f] h-screen clipstyleb'></div>;
+  interface songsI {
+    id: number;
+    name: string;
+    artist: string;
+    artwork: string;
+  }
+  const SONGS: songsI[] = [
+    {
+      id: 1,
+      name: 'Sodus',
+      artist: 'Cemeteries',
+      artwork:
+        'https://e-cdn-images.dzcdn.net/images/cover/483448feaa79f0a832692aa94fc0dea6/264x264-000000-80-0-0.jpg',
+    },
+    {
+      id: 2,
+      name: "It's late",
+      artist: 'A beacon School',
+      artwork:
+        'https://e-cdn-images.dzcdn.net/images/cover/0580f60314ab3e22378d360c192210ea/264x264-000000-80-0-0.jpg',
+    },
+    {
+      id: 3,
+      name: 'Alleyways',
+      artist: 'The neighbourhood',
+      artwork:
+        'https://e-cdn-images.dzcdn.net/images/cover/6f55ff3cc5c478510c948024e95cf8d6/264x264-000000-80-0-0.jpg',
+    },
+  ];
+  return (
+    <div className='w-screen bg-[#93438f] h-screen clipstylemb sm:clipstyleb  '>
+      <aside className='flex flex-col w-screen h-screen  md:flex-row  items-center gap-20 md:gap-48 justify-center '>
+        <div className='h-40  gap-y-10'>
+          <h3 className='text-white text-5xl font-bold w-80'>
+            Listen offline music
+          </h3>
+          <p className='text-white'>
+            Enjoy music without wifi and internet connection
+          </p>
+        </div>
+
+        <div className='flex flex-col gap-6'>
+          {SONGS.map((song: songsI) => (
+            <div
+              className='flex w-80 bg-white p-3 rounded gap-5 items-center'
+              key={song.id}
+            >
+              <div className='flex-shrink-0'>
+                <Image
+                  src={song.artwork}
+                  width={50}
+                  height={50}
+                  alt={'sodus'}
+                />
+              </div>
+              <div className='flex-grow'>
+                <h4 className='font-bold text-md'>{song.name}</h4>
+                <p className='text-xs text-gray-500'>{song.artist}</p>
+              </div>
+              <div className='w-1/6 flex items-center justify-end flex-shrink-0'>
+                <AiFillPlayCircle color='grey' size={30} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </aside>
+    </div>
+  );
 };
 
 export default Offline;
